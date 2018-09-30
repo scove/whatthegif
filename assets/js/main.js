37f114424a208d7e4913e7fd1435e596615a6d58
Chau Tung Lam Nguyen <nctl144@gmail.com>
2018-09-30 01:35:06 -0400

    display the questions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,46 @@
-var GphApiClient = require('giphy-js-sdk-core')
-client = GphApiClient("cUoIFTUc4R2X77xpQZc8AAgKLA0Uvmla")
-/// Gif Search
-client.random('gifs', {})
-.then((response) => {
-  console.log(response)
-})
-.catch((err) => {
+"use strict";
 
-})
+// create your own app on giphy
+var API_KEY = "Uyif5WgGhFtXX7g8Dw0cN7UyiWNhRmdm"
+
+var GphApiClient = require('giphy-js-sdk-core')
+var client = GphApiClient(API_KEY)
+var questions = [
+  "When the code compiles without warnings or errors",
+  "That feeling when the Soylent hits",
+  "The face you make when you think of HackNY",
+  "When your roomate won't stop talking",
+  "After pulling an all-nighter",
+  "When you forget your keys at home"
+]
+var starting_index = 0
+
+function displayQuestion() {
+  document.getElementById("question").innerHTML = questions[0]
+
+  function getQuestion() {
+    starting_index += 1
+    if (starting_index == questions.length) {
+      starting_index = 0
+    }
+    document.getElementById("question").innerHTML = questions[starting_index]
+  }
+
+  document.getElementById("next-question").addEventListener("click", getQuestion)
+}
+
+function getGIF() {
+  /// Gif Search
+  client.random('gifs', {})
+  .then((response) => {
+
+  })
+  .catch((err) => {
+    console.log("there is some error while loading the gif", err)
+  })
+}
+
+(function() {
+  displayQuestion();
+  getGIF();
+})();
